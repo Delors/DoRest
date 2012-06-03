f0d59d41ea40345ef1bc0a354223a6295019bb15
Michael Eichberg <mail@michael-eichberg.de>
2012-06-03 18:36:53 +0200

    applied the "standard" formatting

--- ext/digestauth/src/main/scala/org/dorest/server/auth/DigestAuthentication.scala
+++ ext/digestauth/src/main/scala/org/dorest/server/auth/DigestAuthentication.scala
@@ -19,116 +19,116 @@ package auth
 import java.io.InputStream
 import StringUtils._
 
-/**
- * Implementation of Digest Access Authentication (RFC 2617).
- *
- * @author Mateusz Parzonka
- */
+/** Implementation of Digest Access Authentication (RFC 2617).
+  *
+  * @author Mateusz Parzonka
+  */
 trait DigestAuthentication extends Authentication with Handler {
 
-  private[this] var _authenticatedUser: String = _
+    private[this] var _authenticatedUser: String = _
 
-  def authenticatedUser: String = _authenticatedUser
+    def authenticatedUser: String = _authenticatedUser
 
-  override abstract def processRequest(requestBody: => InputStream): Response = {
-    incomingRequest match {
-      case authorizationRequest: AuthorizationRequest => validate(authorizationRequest) match {
-        case ValidatedRequest => { _authenticatedUser = authorizationRequest.username; super.processRequest(requestBody) }
-        case StaleRequest => unauthorizedDigestResponse(stale = true)
-        case _ => unauthorizedDigestResponse(stale = false)
-      }
-      case _ => unauthorizedDigestResponse(stale = false)
-    }
-  }
-
-  def incomingRequest: Request = {
-
-    requestHeaders.getFirst("Authorization") match {
-      case authorizationHeader: String if authorizationHeader.startsWith("Digest ") => {
-        val m = parseAuthorizationHeader(authorizationHeader)
-        AuthorizationRequest(HTTPMethod.unapply(method), m("username"), m("realm"), m("nonce"), m("uri"), m("qop"), m("nc"), m("cnonce"), m("response"), m("opaque"))
-      }
-      case _ => UnauthorizedRequest
-    }
-  }
-
-  def parseAuthorizationHeader(authorizationHeader: String): Map[String, String] = {
-    val nameValuePairs: List[(String, String)] = splitNameValuePairs(authorizationHeader.substring("Digest ".length))
-    val nameValueMappings: Map[String, String] = nameValuePairs.toMap
-    if (nameValuePairs.length == nameValueMappings.size)
-      nameValueMappings
-    else
-      throw new RequestException(response = BadRequest("Malformed authorization header: " + authorizationHeader))
-  }
-
-  def unauthorizedDigestResponse(stale: Boolean): Response = {
-    val nonce = randomString(64)
-    NonceStorage.addNonce(nonce)
-    UnauthorizedDigestResponse(authenticationRealm, "auth", nonce, randomString(64), stale)
-  }
-
-  def validate(r: AuthorizationRequest): ProcessedAuthorizationRequest = {
-    password(r.username) match {
-      case Some(pwd: String) => {
-        val ha1 = hexEncode(md5(r.username + ":" + r.realm + ":" + pwd))
-        val ha2 = hexEncode(md5(r.method + ":" + r.uri))
-        val response = hexEncode(md5(ha1 + ":" + r.nonce + ":" + r.nc + ":" + r.cnonce + ":" + r.qop + ":" + ha2))
-        (response == r.response) match {
-          case true if (NonceStorage.contains(r.nonce, r.nc)) => ValidatedRequest
-          case true => StaleRequest
-          case _ => UnauthorizedRequest
+    override abstract def processRequest(requestBody: ⇒ InputStream): Response = {
+        incomingRequest match {
+            case authorizationRequest: AuthorizationRequest ⇒ validate(authorizationRequest) match {
+                case ValidatedRequest ⇒ {
+                    _authenticatedUser = authorizationRequest.username
+                    super.processRequest(requestBody)
+                }
+                case StaleRequest ⇒ unauthorizedDigestResponse(stale = true)
+                case _            ⇒ unauthorizedDigestResponse(stale = false)
+            }
+            case _ ⇒ unauthorizedDigestResponse(stale = false)
         }
-      }
-      case _ => UnauthorizedRequest
     }
-  }
+
+    def incomingRequest: Request = {
+
+        requestHeaders.getFirst("Authorization") match {
+            case authorizationHeader: String if authorizationHeader.startsWith("Digest ") ⇒ {
+                val m = parseAuthorizationHeader(authorizationHeader)
+                AuthorizationRequest(HTTPMethod.unapply(method), m("username"), m("realm"), m("nonce"), m("uri"), m("qop"), m("nc"), m("cnonce"), m("response"), m("opaque"))
+            }
+            case _ ⇒ UnauthorizedRequest
+        }
+    }
+
+    def parseAuthorizationHeader(authorizationHeader: String): Map[String, String] = {
+        val nameValuePairs: List[(String, String)] = splitNameValuePairs(authorizationHeader.substring("Digest ".length))
+        val nameValueMappings: Map[String, String] = nameValuePairs.toMap
+        if (nameValuePairs.length == nameValueMappings.size)
+            nameValueMappings
+        else
+            throw new RequestException(response = BadRequest("Malformed authorization header: "+authorizationHeader))
+    }
+
+    def unauthorizedDigestResponse(stale: Boolean): Response = {
+        val nonce = randomString(64)
+        NonceStorage.addNonce(nonce)
+        UnauthorizedDigestResponse(authenticationRealm, "auth", nonce, randomString(64), stale)
+    }
+
+    def validate(r: AuthorizationRequest): ProcessedAuthorizationRequest = {
+        password(r.username) match {
+            case Some(pwd: String) ⇒ {
+                val ha1 = hexEncode(md5(r.username+":"+r.realm+":"+pwd))
+                val ha2 = hexEncode(md5(r.method+":"+r.uri))
+                val response = hexEncode(md5(ha1+":"+r.nonce+":"+r.nc+":"+r.cnonce+":"+r.qop+":"+ha2))
+                (response == r.response) match {
+                    case true if (NonceStorage.contains(r.nonce, r.nc)) ⇒ ValidatedRequest
+                    case true ⇒ StaleRequest
+                    case _ ⇒ UnauthorizedRequest
+                }
+            }
+            case _ ⇒ UnauthorizedRequest
+        }
+    }
 }
 
-/**
- * Thread-safe nonce-storage with background-deletion of expired nonces.
- *
- * @author Mateusz Parzonka
- */
+/** Thread-safe nonce-storage with background-deletion of expired nonces.
+  *
+  * @author Mateusz Parzonka
+  */
 object NonceStorage {
 
-  import scala.collection.JavaConversions._
-  private[this] val nonceMap: scala.collection.mutable.ConcurrentMap[String, (Int, Long)] = new java.util.concurrent.ConcurrentHashMap[String, (Int, Long)](32, 0.75f, 8)
+    import scala.collection.JavaConversions._
+    private[this] val nonceMap: scala.collection.mutable.ConcurrentMap[String, (Int, Long)] = new java.util.concurrent.ConcurrentHashMap[String, (Int, Long)](32, 0.75f, 8)
 
-  val nonceValidityPeriod = 30000 // msec
+    val nonceValidityPeriod = 30000 // msec
 
-  val nonceCleaningInterval = 5000 // msec
+    val nonceCleaningInterval = 5000 // msec
 
-  def addNonce(nonce: String) {
-    nonceMap += (nonce -> (0, System.currentTimeMillis))
-  }
-
-  /**
-   * Checks if the storage contains the given nonce assuring the given nc was not used before.
-   */
-  def contains(nonce: String, nc: String): Boolean = {
-    val curNc = Integer.parseInt(nc, 16)
-    nonceMap.get(nonce) match {
-      case Some((oldNc: Int, time: Long)) if curNc > oldNc => { nonceMap.replace(nonce, (curNc, time)); true }
-      case _ => false
+    def addNonce(nonce: String) {
+        nonceMap += (nonce -> (0, System.currentTimeMillis))
     }
-  }
 
-  def clean() {
-    val currentTime = System.currentTimeMillis
-    for ((nonce, (nc, time)) <- nonceMap if (currentTime - time) > nonceValidityPeriod) nonceMap.-=(nonce)
-  }
-
-  val nonceCleaner = new Thread() {
-    override def run() {
-      while (true) {
-        clean()
-        Thread.sleep(nonceCleaningInterval)
-      }
+    /** Checks if the storage contains the given nonce assuring the given nc was not used before.
+      */
+    def contains(nonce: String, nc: String): Boolean = {
+        val curNc = Integer.parseInt(nc, 16)
+        nonceMap.get(nonce) match {
+            case Some((oldNc: Int, time: Long)) if curNc > oldNc ⇒ { nonceMap.replace(nonce, (curNc, time)); true }
+            case _ ⇒ false
+        }
     }
-  };
-  nonceCleaner.setDaemon(true)
-  nonceCleaner.start()
- 
+
+    def clean() {
+        val currentTime = System.currentTimeMillis
+        for ((nonce, (nc, time)) ← nonceMap if (currentTime - time) > nonceValidityPeriod) nonceMap.-=(nonce)
+    }
+
+    val nonceCleaner = new Thread() {
+        override def run() {
+            while (true) {
+                clean()
+                Thread.sleep(nonceCleaningInterval)
+            }
+        }
+    };
+    nonceCleaner.setDaemon(true)
+    nonceCleaner.start()
+
 }
 
 sealed trait Request
